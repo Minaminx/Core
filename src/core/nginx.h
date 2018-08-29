c17ccbe3000bd1455877a2137253e5f4ddf97c88
NewBugger <contacts@sometimesnaive.org>
2018-08-29 22:44:00 +0800

    Release Update.
    Nginx/1.15.3/master -> Minaminx/2.3.5

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      2003004
-#define NGINX_VERSION      "2.3.4"
+#define nginx_version      2003005
+#define NGINX_VERSION      "2.3.5"
 #define NGINX_VER          "Minaminx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
