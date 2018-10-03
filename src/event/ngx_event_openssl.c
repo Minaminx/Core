72d3a4ff8a0b25a933cd524727fcdca93a5eea41
NewBugger <contacts@sometimesnaive.org>
2018-10-03 20:06:21 +0800

    bugfix: implicit declaration of function 'set_min/max_proto_version'.
    it encounters when min/max equals to 'TLS1_2_VERSION'.

--- src/event/ngx_event_openssl.c
+++ src/event/ngx_event_openssl.c
@@ -354,13 +354,6 @@ ngx_ssl_create(ngx_ssl_t *ssl, ngx_uint_t protocols, void *data)
 #endif
 
 /// Zero is the default version dedided by boringssl.
-#if (NGX_HTTP_SSL_BORINGSSL_TLS_VERSION_MIN)
-    SSL_CTX_set_min_proto_version(ssl->ctx, TLS1_2_VERSION);
-#else
-    SSL_CTX_set_min_proto_version(ssl->ctx, 0);
-#endif
-
-/// Zero is the default version dedided by boringssl.
 #if (NGX_HTTP_SSL_BORINGSSL_TLS_VERSION_MAX)
     #ifdef TLS1_3_VERSION
         SSL_CTX_set_max_proto_version(ssl->ctx, TLS1_3_VERSION);
@@ -369,6 +362,17 @@ ngx_ssl_create(ngx_ssl_t *ssl, ngx_uint_t protocols, void *data)
     #endif
 #else
     SSL_CTX_set_max_proto_version(ssl->ctx, 0);
+#endif
+
+/// Zero is the default version dedided by boringssl.
+#if (NGX_HTTP_SSL_BORINGSSL_TLS_VERSION_MIN)
+    #if (SSL_CTX_set_max_proto_version(ssl->ctx, TLS1_3_VERSION))
+        SSL_CTX_set_min_proto_version(ssl->ctx, TLS1_2_VERSION);
+    #else
+        SSL_CTX_set_min_proto_version(ssl->ctx, 0);
+    #endif
+#else
+    SSL_CTX_set_min_proto_version(ssl->ctx, 0);
 #endif
 
 #ifdef SSL_OP_NO_COMPRESSION
