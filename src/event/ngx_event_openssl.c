b2bb6c648526d86e89938528f10699fd06709f2e
NewBugger <contacts@sometimesnaive.org>
2018-10-03 20:38:15 +0800

    bugfix: implict declaration.

--- src/event/ngx_event_openssl.c
+++ src/event/ngx_event_openssl.c
@@ -365,12 +365,7 @@ ngx_ssl_create(ngx_ssl_t *ssl, ngx_uint_t protocols, void *data)
     #ifdef TLS1_3_VERSION
         SSL_CTX_set_max_proto_version(ssl->ctx, TLS1_3_VERSION);
     #else
-        if (SSL_CTX_set_min_proto_version(ssl->ctx, TLS1_2_VERSION)) {
-            SSL_CTX_set_max_proto_version(ssl->ctx, 0);
-        }
-        else {
-            SSL_CTX_set_max_proto_version(ssl->ctx, TLS1_2_VERSION);
-        }
+        SSL_CTX_set_max_proto_version(ssl->ctx, TLS1_2_VERSION);
     #endif
 #else
     SSL_CTX_set_max_proto_version(ssl->ctx, 0);
