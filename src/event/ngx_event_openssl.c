a812beb7b51bbd01b22762d0d364c50b2c1d91fc
NewBugger <contacts@sometimesnaive.org>
2018-10-03 15:12:02 +0800

    enhancement: CHACHA prefered via nginx rather than OpenSSL.
    via: hakasenyang

--- src/event/ngx_event_openssl.c
+++ src/event/ngx_event_openssl.c
@@ -345,6 +345,7 @@ ngx_ssl_create(ngx_ssl_t *ssl, ngx_uint_t protocols, void *data)
     }
 #endif
 
+/// You can set CHACHA20 priority in nginx without patching OpenSSL.
 #ifdef SSL_OP_PRIORITIZE_CHACHA
     SSL_CTX_set_options(ssl->ctx, SSL_OP_PRIORITIZE_CHACHA);
 #endif
