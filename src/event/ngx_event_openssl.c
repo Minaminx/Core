1ca60bea388a43f0c4386cc6e646dc07fae68912
NewBugger <contacts@sometimesnaive.org>
2018-10-04 18:07:48 +0800

    enhancement: add 'OPENSSL_ON' switch.
    --with-boringssl= -> BORINGSSL_ON
    --with-openssl=   -> OPENSSL_ON

--- src/event/ngx_event_openssl.c
+++ src/event/ngx_event_openssl.c
@@ -347,7 +347,7 @@ ngx_ssl_create(ngx_ssl_t *ssl, ngx_uint_t protocols, void *data)
 
 #if (NGX_BORINGSSL_ON)
     /// Zero is the default version dedided by boringssl.
-    /// --with-boringssl-tls-min=
+    /// --with-boringssl-tls-min=TLS1_2_VERSION
     #ifdef NGX_BORINGSSL_TLS_VERSION_MIN
         SSL_CTX_set_min_proto_version(ssl->ctx, NGX_BORINGSSL_TLS_VERSION_MIN);
     #else
@@ -355,7 +355,7 @@ ngx_ssl_create(ngx_ssl_t *ssl, ngx_uint_t protocols, void *data)
     #endif
 
     /// Zero is the default version dedided by boringssl.
-    /// --with-boringssl-tls-max=
+    /// --with-boringssl-tls-max=TLS1_3_VERSION
     #ifdef NGX_BORINGSSL_TLS_VERSION_MAX
         if (NGX_BORINGSSL_TLS_VERSION_MAX == "TLS1_3_VERSION") {
             #ifdef TLS1_3_VERSION
@@ -372,11 +372,13 @@ ngx_ssl_create(ngx_ssl_t *ssl, ngx_uint_t protocols, void *data)
     #endif
 #endif
 
-/// You can set CHACHA20 priority in nginx without patching OpenSSL.
-/// --with-http_ssl_openssl_chacha_preferred_module
-#if (NGX_HTTP_SSL_OPENSSL_CHACHA_PREFERRED)
-    #ifdef SSL_OP_PRIORITIZE_CHACHA
-        SSL_CTX_set_options(ssl->ctx, SSL_OP_PRIORITIZE_CHACHA);
+#if (NGX_OPENSSL_ON)
+    /// You can set CHACHA20 priority in nginx without patching OpenSSL.
+    /// --with-openssl-chacha-preferred
+    #if (NGX_OPENSSL_CHACHA_PREFERRED)
+        #ifdef SSL_OP_PRIORITIZE_CHACHA
+            SSL_CTX_set_options(ssl->ctx, SSL_OP_PRIORITIZE_CHACHA);
+        #endif
     #endif
 #endif
 
