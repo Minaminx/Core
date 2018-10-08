ad41fb0b100f993c5e12d175251166513271ca9b
NewBugger <contacts@sometimesnaive.org>
2018-10-08 22:13:36 +0800

    Version Update.
    2.3.7.9 -> 2.3.8.0

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -22,8 +22,8 @@
 #define RELEASE_VER        RELEASE_VAR  "/" RELEASE_VERSION
 
 // Version Update (declared)
-#define declared_version   2003007009
-#define DECLARED_VERSION   "2.3.7.9"
+#define declared_version   2003008000
+#define DECLARED_VERSION   "2.3.8.0"
 #define DECLARED_VER       DECLARED_VAR "/" DECLARED_VERSION
 
 // build version ('nginx -V')
@@ -35,8 +35,8 @@
 #define DECLARED_VER_BUILD DECLARED_VER " (" NGX_BUILD ")"
 #define NGINX_VER_BUILD    NGINX_VER    " (" NGX_BUILD ")"
 #else
-#define DECLARED_VER_BUILD DECLARED_VER " (Something Just Like This.)"
-#define NGINX_VER_BUILD    NGINX_VER    " (Something Just Like This.)"
+#define DECLARED_VER_BUILD DECLARED_VER " (Diamond Heart)"
+#define NGINX_VER_BUILD    NGINX_VER    " (Diamond Heart)"
 #endif
 
 
